9280b9975e8f8aa35334271e89155fe23526cab6
mrynp <110476086+mrynp@users.noreply.github.com>
2024-04-20 15:20:57 +0800

    Add weather component

--- src/App.js
+++ src/App.js
@@ -1,71 +1,20 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React from "react";
+import Weather from "./Weather";
 
 function App() {
-  const [location, setLocation] = useState("");
-  const [weather, setWeather] = useState({});
-
-  function displayWeather(response) {
-    console.log(response.data.name);
-    setWeather({
-      name: response.data.name,
-      temperature: Math.round(response.data.main.temp),
-      humidity: response.data.main.humidity,
-      wind: response.data.wind.speed,
-      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
-      description: response.data.weather[0].description,
-    });
-  }
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    let key = "8cac06f7ab6c10287cd06a316ff84a57";
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${key}&units=metric`;
-    axios.get(url).then(displayWeather);
-  }
-
-  function updateCity(event) {
-    setLocation(event.target.value);
-  }
-
   return (
     <div className="app">
-      <form className="search" onSubmit={handleSubmit}>
-        <input
-          value={location}
-          onChange={updateCity}
-          placeholder="Enter location"
-          type="search"
-        />
-      </form>
-      <div className="container">
-        <div className="top">
-          <div className="left">
-            <div className="location">
-              <p>{weather.name}</p>
-            </div>
-
-            <div className="temp">
-              <p>
-                {weather.temperature}
-                <span className="unit">°C</span>
-              </p>
-            </div>
-          </div>
-          <div className="right">
-            <div className="icon">
-              <img src={weather.icon} alt={weather.description} />
-            </div>
-            <p className="details">
-              <span className="description">{weather.description}</span>
-              <br />
-              <span className="humidity">Humidity: {weather.humidity}%, </span>
-              <span className="humidity">Wind: {weather.wind}km/h</span>
-            </p>
-          </div>
-        </div>
-        <div className="bottom"></div>
-      </div>
+      <Weather defaultCity="Manila" />
+      <footer>
+        This project was coded by Marienne and is{" "}
+        <a
+          href="https://github.com/mrynp/weather-project-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function Weather(props) {
+  const [location, setLocation] = useState(props.defaultCity);
+  const [weather, setWeather] = useState({});
+  const [ready, setReady] = useState(false);
+
+  function displayWeather(response) {
+    console.log(response.data.name);
+    setWeather({
+      name: response.data.name,
+      temperature: Math.round(response.data.main.temp),
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      description: response.data.weather[0].description,
+    });
+    setReady(true);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function updateCity(event) {
+    setLocation(event.target.value);
+  }
+
+  function search() {
+    let key = "8cac06f7ab6c10287cd06a316ff84a57";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${key}&units=metric`;
+    axios.get(url).then(displayWeather);
+  }
+
+  if (ready) {
+    return (
+      <>
+        <form className="search" onSubmit={handleSubmit}>
+          <input
+            value={location}
+            onChange={updateCity}
+            placeholder="Enter location"
+            type="search"
+          />
+        </form>
+        <div className="container">
+          <div className="top">
+            <div className="left">
+              <div className="location">
+                <p>{weather.name}</p>
+              </div>
+
+              <div className="temp">
+                <p>
+                  {weather.temperature}
+                  <span className="unit">°C</span>
+                </p>
+              </div>
+            </div>
+            <div className="right">
+              <div className="icon">
+                <img src={weather.icon} alt={weather.description} />
+              </div>
+              <p className="details">
+                <span className="description">{weather.description}</span>
+                <br />
+                <span className="humidity">
+                  Humidity: {weather.humidity}%,{" "}
+                </span>
+                <span className="humidity">Wind: {weather.wind}km/h</span>
+              </p>
+            </div>
+          </div>
+          <div className="bottom"></div>
+        </div>
+      </>
+    );
+  } else {
+    search();
+  }
+}
