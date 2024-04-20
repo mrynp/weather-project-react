d88df2f2707a5da31f7bd0d4a9d7dc4bdd0a975c
mrynp <110476086+mrynp@users.noreply.github.com>
2024-04-20 12:55:55 +0800

    Add weather app elements

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React from "react";
+// import axios from "axios";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="app">
+      <form className="search">
+        <input placeholder="Enter location" type="search" />
+      </form>
+      <div className="container">
+        <div className="top">
+          <div className="left">
+            <div className="location">
+              <p>Manila</p>
+            </div>
+            <div className="temp">
+              <p>28°C</p>
+            </div>
+          </div>
+          <div className="right">
+            <div className="icon"></div>
+            <p className="details">
+              <span className="description">Sunny</span>
+              <br />
+              <span className="humidity">Humidity: 87%, </span>
+              <span className="humidity">Wind: 4.5km/h</span>
+            </p>
+          </div>
+        </div>
+        <div className="bottom"></div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
